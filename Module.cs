4cf1b091e4bd63814d067aa01988241d99b99b30
Pixelgel <pixelgel@outlook.com>
2015-02-15 22:10:47 -0600

    Removed some unnecessary dependencies.

--- Module.cs
+++ Module.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.FSharp.Collections;
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
